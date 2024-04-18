8b3c714e0aae7180310aec2c315bab38e26ffa20
gustavocarus0_ <89716464+GustavoCaruso@users.noreply.github.com>
2024-04-18 20:31:37 -0300

    Test

---
+++ tests/ToledoCW.Services.UnitTest.Domain/Entidades/AtendenteTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToledoCW.Services.Infraestructure.Entidades;
+
+namespace ToledoCW.Services.UnitTest.Domain.Entidades
+{
+    public class AtendenteTest
+    {
+        [Fact]
+        public void EhValido_DeveValidarInstancia()
+        {
+            //Arrange
+            var _obj = new Atendente();
+            _obj.Nome = "Gustavo";
+            _obj.Estabelecimento = 1;
+
+            //Act
+            var _result = _obj.EhValido();
+
+            //Assert
+            Assert.True(_result );
+        }
+    }
+}
